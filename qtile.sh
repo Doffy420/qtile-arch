c8e18432e232c023c07bf4daf1f702e76b4cfbe2
Doffy420 <107606763+Doffy420@users.noreply.github.com>
2022-07-09 20:05:10 +0800

    Add files via upload

---
+++ qtile.sh
@@ -0,0 +1,6 @@
+sudo pacman -S qtile pacman-contrib
+yay -S nerd-fonts-ubuntu-mono
+pip install psutil
+git clone https://github.com/antoniosarosi/dotfiles.git
+cp -r dotfiles/.config/qtile ~/.config
+
